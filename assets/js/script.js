af6818611425b891a69756ec21ee91a86031af8c
Paulina Flores <fosp.010715@gmail.com>
2026-02-23 00:09:28 -0600

    Se agregan secciones de galeria

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+const slides = document.querySelectorAll(".slide");
+const indicator = document.getElementById("indicator");
+
+let index = 0;
+
+function update() {
+  slides.forEach((s, i) => {
+    s.classList.toggle("active", i === index);
+  });
+
+  indicator.textContent = `${index + 1} / ${slides.length}`;
+}
+
+document.getElementById("next").onclick = () => {
+  if (index < slides.length - 1) index++;
+  update();
+};
+
+document.getElementById("prev").onclick = () => {
+  if (index > 0) index--;
+  update();
+};
+
+// WhatsApp link dinámico
+const waNumber = "522212706617";
+const waMessage = "¡Hola! Confirmo con mucha emoción mi asistencia al cumpleaños de Nasya Mailen ¡Nos vemos en su gran día!";
+
+document.getElementById("waLink").href =
+  `https://wa.me/${waNumber}?text=${encodeURIComponent(waMessage)}`;
+
+update();
+
+const intro = document.getElementById("introScreen");
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+const musicIcon = document.getElementById("musicIcon");
+
+intro.addEventListener("click", () => {
+
+  music.volume = 0.5;
+  music.play();
+
+  musicBtn.classList.add("playing");
+  musicIcon.src = "assets/img/pause.svg";
+
+  intro.classList.add("hidden");
+
+  // Opcional: eliminar del DOM después de animación
+  setTimeout(() => {
+    intro.remove();
+  }, 600);
+
+});
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const cerrar = document.getElementById("cerrar");
+
+document.querySelectorAll(".galeria img").forEach(img => {
+
+  img.style.cursor = "pointer";
+
+  img.addEventListener("click", function (e) {
+    e.stopPropagation(); // evita conflictos futuros
+    lightbox.classList.add("active");
+    lightboxImg.src = this.src;
+  });
+
+});
+
+cerrar.addEventListener("click", function (e) {
+  e.stopPropagation();
+  lightbox.classList.remove("active");
+});
+
+lightbox.addEventListener("click", function () {
+  lightbox.classList.remove("active");
+});
+
+const particlesContainer = document.querySelector(".particles");
+
+for (let i = 0; i < 25; i++) {
+  const particle = document.createElement("span");
+  
+  particle.style.left = Math.random() * 100 + "vw";
+  particle.style.animationDuration = (6 + Math.random() * 6) + "s";
+  particle.style.animationDelay = Math.random() * 5 + "s";
+  particle.style.width = particle.style.height = (4 + Math.random() * 6) + "px";
+  
+  particlesContainer.appendChild(particle);
+}
